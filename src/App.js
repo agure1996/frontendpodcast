015bb1e8881122ef56a90d214f38b3e643bc3ea6
greggbaker <abbasgure@googlemail.com>
2023-12-18 19:00:22 +0000

    axios wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import Employees from './components/Employees.jsx'
 import './App.css';
+const cors = require('cors')
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <h1> Testing Axios produces the employee json body</h1>
+      <Employees />
     </div>
   );
 }
