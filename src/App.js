2fbe567424f3eb9825c710586639a7c9feac8a62
greggbaker <abbasgure@googlemail.com>
2024-01-07 17:59:11 +0000

    json stringify response worked but not proper solution

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import Employees from './components/Employees.jsx'
+
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 const cors = require('cors')
 
 
 function App() {
+  
+  
+  const [employees, setEmployees] = useState('')
+  
+  const getEmployees = (e) =>{
+    
+    const BASE_URL = 'http://localhost:8080/employees/getAllEmployees'
+    axios.get(BASE_URL)
+    .then(response => {
+        console.log(response.data)
+        setEmployees(JSON.stringify(response.data))
+      }).catch(err => console.log(err))
+  }
+
+
+
   return (
     <div className='App'>
 
       <h1> Testing Axios produces the employee json body</h1>
-      <Employees />
+      <button onClick={getEmployees}> Get Employees </button>
+      {employees && <p>{employees}</p>}
     </div>
   );
 }
